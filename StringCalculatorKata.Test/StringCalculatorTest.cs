8cab672eefd9459dbfc0f121a2e4937e44f27a23
Alistair McKinnell <alistair.mckinnell@gmail.com>
2013-09-12 00:37:12 -0400

    Initial commit.
    Implement the first two requirements: create a simple Add() method that
    sums an arbitrary list of numbers.

--- StringCalculatorKata.Test/StringCalculatorTest.cs
+++ StringCalculatorKata.Test/StringCalculatorTest.cs
@@ -12,5 +12,20 @@ namespace StringCalculatorKata.Test
         {
             Assert.AreEqual(0, sut.Add(""));
         }
+
+        [TestMethod]
+        public void AddsSingleNumberToItself()
+        {
+            Assert.AreEqual(1, sut.Add("1"));
+            Assert.AreEqual(2, sut.Add("2"));
+        }
+
+        [TestMethod]
+        public void AddsMultipleNumbersToSum()
+        {
+            Assert.AreEqual(3, sut.Add("1,2"));
+            Assert.AreEqual(5, sut.Add("2,3"));
+            Assert.AreEqual(10, sut.Add("1,2,3,4"));
+        }
     }
 }
--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -1,10 +1,29 @@
-﻿namespace StringCalculatorKata
+﻿using System;
+
+namespace StringCalculatorKata
 {
     public class StringCalculator
     {
         public int Add(string numbers)
         {
-            return -1;
+            int sum = 0;
+
+            foreach (var number in Parse(numbers))
+            {
+                sum += ToNumber(number);
+            }
+
+            return sum;
+        }
+
+        private string[] Parse(string numbers)
+        {
+            return numbers.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private int ToNumber(string number)
+        {
+            return int.Parse(number);
         }
     }
 }
