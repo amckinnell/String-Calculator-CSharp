cc88a215f30efc7744b9aa04a49fc68f74c8ddfb
Alistair McKinnell <alistair.mckinnell@gmail.com>
2013-09-12 00:32:36 -0400

    Initial commit.

---
+++ StringCalculatorKata.Test/StringCalculatorTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculatorKata.Test
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        private readonly StringCalculator sut = new StringCalculator();
+
+        [TestMethod]
+        public void AddsEmptyNumbersToZero()
+        {
+            Assert.AreEqual(0, sut.Add(""));
+        }
+    }
+}
---
+++ StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            return -1;
+        }
+    }
+}
+
